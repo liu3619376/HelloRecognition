3e69cc79b83493c9b5a87d5847ced98b3c0221cf
liuyang <liuyang@liuyangdeMacBook-Pro.local>
2017-12-04 10:14:36 +0800

    Hello

---
+++ HelloRecogition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloRecogition
+//
+//  Created by liuyang on 2017/12/4.
+//  Copyright © 2017年 liuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloRecogition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloRecogition
+//
+//  Created by liuyang on 2017/12/4.
+//  Copyright © 2017年 liuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
